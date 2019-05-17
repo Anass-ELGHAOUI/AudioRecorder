1f74404da26171344a7a0b28a7aea0f87d264cba
Anass-ELGHAOUI <elghaoui.anass@gmail.com>
2019-05-17 00:41:22 +0000

    changing the displayin method

---
+++ app/src/main/java/com/example/anass/audiorecorder/Fragments/NVDisplayImpRecordFragment.java
@@ -0,0 +1,220 @@
+package com.example.anass.audiorecorder.Fragments;
+
+import android.annotation.TargetApi;
+        import android.app.Fragment;
+        import android.media.MediaPlayer;
+        import android.net.Uri;
+        import android.os.Build;
+        import android.os.Bundle;
+        import android.os.CountDownTimer;
+        import android.speech.tts.TextToSpeech;
+        import android.support.annotation.Nullable;
+        import android.util.Log;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ImageView;
+
+        import com.example.anass.audiorecorder.Activities.MainActivity;
+        import com.example.anass.audiorecorder.Helper.OnSwipeTouchListener;
+        import com.example.anass.audiorecorder.Helper.Utils;
+        import com.example.anass.audiorecorder.Models.ImportantRecord;
+        import com.example.anass.audiorecorder.Models.RecordingItem;
+        import com.example.anass.audiorecorder.R;
+
+        import java.io.IOException;
+        import java.util.HashMap;
+        import java.util.Locale;
+
+        import butterknife.Bind;
+        import butterknife.ButterKnife;
+        import butterknife.OnLongClick;
+
+        import static butterknife.ButterKnife.bind;
+        import static butterknife.ButterKnife.unbind;
+
+public class NVDisplayImpRecordFragment extends Fragment{
+
+    @Bind(R.id.iv_nv_choice)
+    public ImageView ivChoice;
+
+    MainActivity activity;
+    private TextToSpeech mTTS;
+    private MediaPlayer mediaPlayer;
+    private RecordingItem recordingItem;
+    private ImportantRecord importantRecord;
+
+    public static final String IMPRECORD_ARGS = "impRecord";
+    public static final String RECORDITEM_ARGS = "recordItem";
+
+    public static NVDisplayImpRecordFragment newInstance(ImportantRecord importantRecord, RecordingItem recordingItem) {
+        NVDisplayImpRecordFragment fragment = new NVDisplayImpRecordFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(IMPRECORD_ARGS,importantRecord);
+        args.putSerializable(RECORDITEM_ARGS,recordingItem);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.nv_choice_fragment, container, false);
+        bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.i("nv choice fragment", "lunched");
+        activity = (MainActivity) getActivity();
+        recordingItem = (RecordingItem) getArguments().getSerializable(RECORDITEM_ARGS);
+        importantRecord = (ImportantRecord) getArguments().getSerializable(IMPRECORD_ARGS);
+        init();
+    }
+
+    public void init(){
+        textToSpeechConfiguration();
+        swipeConfiguration();
+    }
+
+    private void swipeConfiguration(){
+        ivChoice.setOnTouchListener(new OnSwipeTouchListener(activity) {
+            public void onSwipeTop() {
+                if (mTTS.isSpeaking()) {
+                    mTTS.stop();
+                    mTTS.shutdown();
+                }
+                if(mediaPlayer != null && mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    textToSpeechConverter("pour lancer le record glisser vers le haut, pour supprimer le record important glisser vers la gauche, pour retourner glisser vers le bas.");
+                }else{
+                    displayRecord();
+                }
+
+            }
+            public void onSwipeRight() {
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    textToSpeechConverter("pour lancer le record glisser vers le haut, pour supprimer le record important glisser vers la gauche, pour retourner glisser vers le bas.");
+                }
+            }
+            public void onSwipeLeft() {
+                if (mTTS.isSpeaking()) {
+                    mTTS.stop();
+                    mTTS.shutdown();
+                }
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    textToSpeechConverter("pour lancer le record glisser vers le haut, pour supprimer le record important glisser vers la gauche, pour retourner glisser vers le bas.");
+                }
+               //delete record
+            }
+            public void onSwipeBottom() {
+                if (mTTS.isSpeaking()) {
+                    mTTS.stop();
+                    mTTS.shutdown();
+                }
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    textToSpeechConverter("pour lancer le record glisser vers le haut, pour supprimer le record important glisser vers la gauche, pour retourner glisser vers le bas.");
+                }
+                else{
+                    activity.onBackPressed();
+                }
+
+            }
+
+        });
+    }
+
+    private void displayRecord() {
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.reset();
+            mediaPlayer = null;
+        } else {
+            mediaPlayer = null;
+        }
+        mediaPlayer = new MediaPlayer();
+
+        try {
+            mediaPlayer.setDataSource(activity.getApplicationContext(), Uri.parse(recordingItem.getFilePath()));
+            mediaPlayer.prepare();
+            mediaPlayer.seekTo((int) (importantRecord.getStartTime() - recordingItem.getStart()));
+            mediaPlayer.start();
+            new CountDownTimer(importantRecord.getStopTime() - importantRecord.getStartTime(), 1000) {
+
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void onFinish() {
+                    // TODO Auto-generated method stub
+                    if (mediaPlayer.isPlaying()) {
+                        mediaPlayer.stop();
+                    }
+
+                }
+            }.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void textToSpeechConfiguration(){
+        mTTS = new TextToSpeech(activity, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = mTTS.setLanguage(Locale.FRANCE);
+
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "Language not supported");
+                    } else {
+                        textToSpeechConverter("pour lancer le record glisser vers le haut, pour supprimer le record important glisser vers la gauche, pour retourner glisser vers le bas.");                    }
+                } else {
+                    Log.e("TTS", "Initialization failed");
+                }
+            }
+        });
+    }
+
+    private void textToSpeechConverter(String text) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ttsGreater21(text);
+        } else {
+            ttsUnder20(text);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private void ttsUnder20(String text) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "MessageId");
+        mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, map);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void ttsGreater21(String text) {
+        String utteranceId=this.hashCode() + "";
+        mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unbind(this);
+        if (mTTS != null) {
+            mTTS.stop();
+            mTTS.shutdown();
+        }
+    }
+}
+
--- app/src/main/java/com/example/anass/audiorecorder/Fragments/NVImpRecordsListFragment.java
+++ app/src/main/java/com/example/anass/audiorecorder/Fragments/NVImpRecordsListFragment.java
@@ -1,13 +1,17 @@
 package com.example.anass.audiorecorder.Fragments;
 
+import android.annotation.TargetApi;
 import android.app.Fragment;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.anass.audiorecorder.Activities.MainActivity;
 import com.example.anass.audiorecorder.Adapters.ImpRecordsAdapter;
@@ -15,25 +19,36 @@ import com.example.anass.audiorecorder.Adapters.NVImpRecordsAdapter;
 import com.example.anass.audiorecorder.Database.DataBase;
 import com.example.anass.audiorecorder.Database.Repositories.ImportantRecordRepository;
 import com.example.anass.audiorecorder.Helper.OnLoadCompleted;
+import com.example.anass.audiorecorder.Helper.OnSwipeTouchListener;
 import com.example.anass.audiorecorder.Managers.RecyclerViewManager;
 import com.example.anass.audiorecorder.Models.ImportantRecord;
 import com.example.anass.audiorecorder.Models.RecordingItem;
 import com.example.anass.audiorecorder.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+import static butterknife.ButterKnife.unbind;
+
 
 public class NVImpRecordsListFragment extends Fragment implements OnLoadCompleted {
 
-    @Bind(R.id.imp_records_recycler)
-    public RecyclerView mainRecycler;
+    @Bind(R.id.iv_imp_record_list)
+    public ImageView ivImpList;
+
 
     MainActivity activity;
-    NVImpRecordsAdapter adapter;
+    private List<ImportantRecord> importantRecordList = new ArrayList<>();
+    private static int listPosition = 0;
+    private int sizeList = 0;
+    private TextToSpeech mTTS;
+
     ImportantRecordRepository.getRecordsAsyncTask recordsAsyncTask;
     DataBase db;
     private int idRecord;
@@ -53,7 +68,7 @@ public class NVImpRecordsListFragment extends Fragment implements OnLoadComplete
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.important_records_list_fragment, container, false);
+        View view = inflater.inflate(R.layout.nv_important_record_list_fragment, container, false);
         ButterKnife.bind(this, view);
         return view;
     }
@@ -69,10 +84,108 @@ public class NVImpRecordsListFragment extends Fragment implements OnLoadComplete
     public void init() {
         idRecord = getArguments().getInt(ARGS_ID);
         recordingItem = (RecordingItem) getArguments().getSerializable(ARGS_ITEM);
-        adapter = new NVImpRecordsAdapter(activity,recordingItem);
-        RecyclerViewManager.configureRecycleView(activity, mainRecycler);
-        mainRecycler.setAdapter(adapter);
         getData();
+        textToSpeechConfiguration();
+        swipeConfiguration();
+    }
+
+    private void swipeConfiguration(){
+        ivImpList.setOnTouchListener(new OnSwipeTouchListener(activity) {
+            public void onSwipeTop() {
+                if (mTTS.isSpeaking()) {
+                    mTTS.stop();
+                    mTTS.shutdown();
+                }
+                if(sizeList!=0){
+                    activity.navigateTo(NVDisplayImpRecordFragment.newInstance(importantRecordList.get(listPosition),recordingItem));
+                }
+            }
+            public void onSwipeRight() {
+                if (mTTS.isSpeaking()) {
+                    mTTS.stop();
+                    mTTS.shutdown();
+                }
+                if(sizeList!=0) {
+                    listPosition = (listPosition + 1) % sizeList;
+                    textToSpeechConverter("Record Important " + listPosition + ". pour choisir ce record glisser vers le haut, glisser vers la droite pour avancer, glisser vers la gauche pour revenir en arriére. glisser vers le bas pour retourner.");
+                }
+            }
+            public void onSwipeLeft() {
+                if (mTTS.isSpeaking()) {
+                    mTTS.stop();
+                    mTTS.shutdown();
+                }
+                if(sizeList!=0){
+                    listPosition = (listPosition - 1 + sizeList )%sizeList;
+                    textToSpeechConverter("Record Important "+listPosition+". pour choisir ce record glisser vers le haut, glisser vers la droite pour avancer, glisser vers la gauche pour revenir en arriére. glisser vers le bas pour retourner.");
+                }
+            }
+            public void onSwipeBottom() {
+                if (mTTS.isSpeaking()) {
+                    mTTS.stop();
+                    mTTS.shutdown();
+                }
+                activity.onBackPressed();
+            }
+
+        });
+    }
+
+    private void textToSpeechConfiguration(){
+        mTTS = new TextToSpeech(activity, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = mTTS.setLanguage(Locale.FRANCE);
+
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "Language not supported");
+                    } else {
+                        if(sizeList==0){
+                            textToSpeechConverter("pas de record important. glisser vers le bas pour retourner.");
+                        }else{
+                            textToSpeechConverter("Record Important "+listPosition+". pour choisir ce record glisser vers le haut, glisser vers la droite pour avancer, glisser vers la gauche pour revenir en arriére. glisser vers le bas pour retourner.");
+                        }
+
+                    }
+                } else {
+                    Log.e("TTS", "Initialization failed");
+                }
+            }
+        });
+    }
+
+
+    private void textToSpeechConverter(String text) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ttsGreater21(text);
+        } else {
+            ttsUnder20(text);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private void ttsUnder20(String text) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "MessageId");
+        mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, map);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void ttsGreater21(String text) {
+        String utteranceId=this.hashCode() + "";
+        mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unbind(this);
+        if (mTTS != null) {
+            mTTS.stop();
+            mTTS.shutdown();
+        }
     }
 
     @Override
@@ -89,7 +202,8 @@ public class NVImpRecordsListFragment extends Fragment implements OnLoadComplete
     private void refreshRecyclerView() {
         List<ImportantRecord> recordingItems = recordsAsyncTask.getImportantRecords();
         if (recordingItems != null && recordingItems.size() > 0) {
-            adapter.addAllItems(recordingItems);
+            importantRecordList = recordingItems;
+            sizeList = importantRecordList.size();
         }
     }
 
